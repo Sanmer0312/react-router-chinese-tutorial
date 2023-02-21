9c2a51eeac734f95f6b5d1de49ec48b101b27ba0
tangzhisheng <tangzhisheng@gzgxinfo.com>
2023-02-21 16:04:29 +0800

    update demo

--- demo/src/routes/edit.jsx
+++ demo/src/routes/edit.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Form, useLoaderData, redirect } from "react-router-dom";
+import { Form, useLoaderData, redirect, useNavigate } from "react-router-dom";
 import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
@@ -10,7 +9,8 @@ export async function action({ request, params }) {
 }
 
 export default function EditContact() {
-    const { contact } = useLoaderData() as { contact: Record<string, any> };
+    const { contact } = useLoaderData();
+    const navigate = useNavigate();
 
     return (
         <Form method="post" id="contact-form">
@@ -60,7 +60,13 @@ export default function EditContact() {
             </label>
             <p>
                 <button type="submit">Save</button>
-                <button type="button">Cancel</button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        navigate(-1);
+                    }}>
+                    Cancel
+                </button>
             </p>
         </Form>
     );
